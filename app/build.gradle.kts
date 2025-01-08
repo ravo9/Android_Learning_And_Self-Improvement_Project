188675b490f618a9cbebcca3dd1e81208add2830
Rafal Ozog <rafalozog@gmail.com>
2025-01-08 14:52:46 +0000

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.dreamcatcher.travelwithai"
         minSdk = 25
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
